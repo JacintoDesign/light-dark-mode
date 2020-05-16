e2d243ecbbed17e0be7b7bf366616abb723d60cf
JACINTO WONG <jacinto.wong@cbc.ca>
2020-05-16 17:43:32 -0400

    final production code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
@@ -7,28 +7,29 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const columns = document.getElementById('columns');
 
+// Dark or Light Images
+function imageMode(color) {
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
 // Dark Mode Styles
 function darkMode() {
-  nav.style.backgroundColor = 'rgba(0,0,0,.5)';
-  columns.style.backgroundColor = 'rgba(255,255,255,.5)';
-  toggleIcon.children[0].innerText = 'Dark Mode';
-  toggleIcon.children[1].classList.remove('fa-sun');
-  toggleIcon.children[1].classList.add('fa-moon');
-  image1.src = 'img/undraw_proud_coder_dark.svg';
-  image2.src = 'img/undraw_feeling_proud_dark.svg';
-  image3.src = 'img/undraw_conceptual_idea_dark.svg';
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  columns.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
 }
 
 // Light Mode Styles
 function lightMode() {
-  nav.style.backgroundColor = 'rgba(255,255,255,.5)';
-  columns.style.backgroundColor = 'rgba(0,0,0,.5)';
-  toggleIcon.children[0].innerText = 'Light Mode';
-  toggleIcon.children[1].classList.remove('fa-moon');
-  toggleIcon.children[1].classList.add('fa-sun');
-  image1.src = 'img/undraw_proud_coder_light.svg';
-  image2.src = 'img/undraw_feeling_proud_light.svg';
-  image3.src = 'img/undraw_conceptual_idea_light.svg';
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  columns.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode('light');
 }
 
 // Check For Theme
@@ -55,4 +56,4 @@ function switchTheme(e) {
 }
 
 // Event Listener
-toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitch.addEventListener('change', switchTheme);
